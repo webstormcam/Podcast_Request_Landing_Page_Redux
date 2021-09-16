23ec3a744d1e57e79e340ea6ede1ee1cd75684df
Cameron Chandler <webstormcam@gmail.com>
2021-09-15 21:12:37 -0400

    Added in the proper JS code

---
+++ script.js
@@ -0,0 +1,18 @@
+function validateForm(){
+    let x=document.getElementById('email').value;
+    let text;
+    if (x ===''){
+        text=' Oops! Please add your email'
+    
+    } else if (x.includes('@')==false){
+        text='Oops! Please check your email'
+    
+    }else if (x.length<5 && x.includes('@')===true){
+        text='Oops! Please check your email'
+   
+    } else if (x.length>=6 && x.includes('@')===true){
+        text=''
+        document.getElementById("myForm").submit();
+     }
+    document.getElementById('error').innerHTML=text;
+}
